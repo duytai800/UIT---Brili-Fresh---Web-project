d67d4ffcbea1b8578f9ffeb201639ad19a1224b4
duytai800 <duytai800@gmail.com>
2022-12-23 09:13:46 +0700

    host db

--- config/database.php
+++ config/database.php
@@ -43,33 +43,33 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'brili_fresh'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            // 'charset' => 'utf8mb4',
-            'charset' => 'utf8',
-            'collation' => 'utf8_general_ci',
+        //  'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'brili_fresh'),
+        //     'username' => env('DB_USERNAME', 'root'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     // 'charset' => 'utf8mb4',
+        //     'charset' => 'utf8',
+        //     'collation' => 'utf8_general_ci',
             
-            //'collation' => 'utf8_unicode_ci  ',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-/*
+        //     //'collation' => 'utf8_unicode_ci  ',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '34.29.1.79'),
+            'host' => env('DB_HOST', '34.87.2.4'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'brili_fresh'),
             'username' => env('DB_USERNAME', 'brilifreshdb'),
@@ -88,7 +88,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-*/
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
